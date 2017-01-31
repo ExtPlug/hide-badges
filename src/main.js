a337dc530cebcaa4a3d928b61349c1cddb4f2133
René Kooi <renee@kooi.me>
2017-01-31 12:41:54 +0100

    ES Modulesify.

--- src/main.js
+++ src/main.js
@@ -1,54 +1,35 @@
-define(function (require, exports, module) {
+import Plugin from 'extplug/Plugin';
+import Events from 'plug/core/Events';
+import style from './style.css';
 
-  const Plugin = require('extplug/Plugin');
-  const Events = require('plug/core/Events');
+const HideBadges = Plugin.extend({
+  name: 'Hide Badges',
+  description: 'Hides user chat badges.',
 
-  const HideBadges = Plugin.extend({
-    name: 'Hide Badges',
-    description: 'Hides user chat badges.',
+  settings: {
+    inline: { type: 'boolean', label: 'Inline Chat', default: false }
+  },
 
-    settings: {
-      inline: { type: 'boolean', label: 'Inline Chat', default: false }
-    },
+  style,
 
-    style: {
-      '#chat': {
-        '.msg': {
-          'padding': '5px 8px 6px 8px'
-        },
-        '.badge-box': {
-          // can't just display:none, because the user rollover positions itself
-          // relative to the badge box
-          'visibility': 'hidden',
-          'width': 0,
-          'margin-left': 0
-        },
-        '.cm.inline .badge-box': {
-          'margin-left': 0,
-          'margin-right': 0
-        }
-      }
-    },
+  enable() {
+    this._super();
+    Events.on('chat:afterreceive', this.onMessage, this);
+  },
 
-    enable() {
-      this._super();
-      Events.on('chat:afterreceive', this.onMessage, this);
-    },
+  disable() {
+    this._super();
+    Events.off('chat:afterreceive', this.onMessage, this);
+  },
 
-    disable() {
-      this._super();
-      Events.off('chat:afterreceive', this.onMessage, this);
-    },
-
-    onMessage(message, el) {
-      if (!this.settings.get('inline')) return;
-      if ([ 'message', 'moderation', 'mention', 'emote' ].indexOf(message.type) !== -1) {
-        el.addClass('inline');
-      }
+  onMessage(message, el) {
+    if (!this.settings.get('inline')) {
+      return;
     }
-
-  });
-
-  module.exports = HideBadges;
-
+    if (['message', 'moderation', 'mention', 'emote'].indexOf(message.type) !== -1) {
+      el.addClass('inline');
+    }
+  }
 });
+
+export default HideBadges;
